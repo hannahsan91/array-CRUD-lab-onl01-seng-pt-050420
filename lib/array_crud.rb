a5a698d1a8fd5137798215583432ad58cc7d2a56
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-09 00:30:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,11 +11,11 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-
+  LGBT_community.unshift("members")
 end
 
 def remove_element_from_end_of_array(array)
-
+  LGBT_community
 end
 
 def remove_element_from_start_of_array(array)
