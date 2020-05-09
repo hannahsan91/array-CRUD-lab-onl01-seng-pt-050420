b83174a48a1a3490fba46ac4bb2757377b383568
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-09 00:33:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,33 +3,33 @@ def create_an_empty_array
 end
 
 def create_an_array
-  LGBT_community = ["lesbian", "gay", "bisexual", "transgender"]
+  community = ["lesbian", "gay", "bisexual", "transgender"]
 end
 
 def add_element_to_end_of_array(array, element)
-  LGBT_community << "questioning"
+  community << "questioning"
 end
 
 def add_element_to_start_of_array(array, element)
-  LGBT_community.unshift("members")
+  community.unshift("members")
 end
 
 def remove_element_from_end_of_array(array)
-  LGBT_community.pop
+  community.pop
 end
 
 def remove_element_from_start_of_array(array)
-  LGBT_community.shift
+  community.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  LGBT_community[1]
+  community[1]
 end
 
 def retrieve_first_element_from_array(array)
-  LGBT_community[0]
+  community[0]
 end
 
 def retrieve_last_element_from_array(array)
-  LGBT_community[-1]
+  community[-1]
 end
