1ec661164124c4d39ae599e6102cb2ae0a65ed5a
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-09 00:31:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,11 +23,11 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  LGBT_community[1]
 end
 
 def retrieve_first_element_from_array(array)
-
+  LGBT_community[0]
 end
 
 def retrieve_last_element_from_array(array)
